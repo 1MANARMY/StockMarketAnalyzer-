0f5cb0d89b4a314833469e37bcbc965647a23b88
Edwin <ecarra7979@gmail.com>
2016-01-17 17:51:22 -0500

    Configured main.py, still need to understand the structure more

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-import matplotlib.pyplot as plt
-from sklearn import datasets
-from sklearn import svm
+import pandas as pd
+import os
+import time
+from datetime import datetime
 
-digits = datasets.load_digits()
-clf = svm.SVC(gamma=0.0001, C=100)
-X,y = digits.data[:-10], digits.target[:-10]
-clf.fit(X,y)
-print(clf.predict(digits.data[-5]))
-plt.imshow(digits.images[-5], cmap=plt.cm.gray_r, interpolation='nearest')
-plt.show()
+path = "/Users/edwincarrasquillo/Git/StockMarketAnalyzer-/intraQuarter"
+
+def Key_Stats(gather="Total Debt/Equity (mrq)"):
+    statspath = path+'/_KeyStats'
+    stock_list = [x[0] for x in os.walk(statspath)]
+
+    for each_dir in stock_list[1:]:
+        each_file = os.listdir(each_dir)
+	ticker = each.dirsplit("\\")[1]
+	if len(each_file) > 0:
+	    for file in each_file:
+		date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
+		unix_time = time.mktime(date_stamp.timetuple())
+		print(date_stamp, unix_time)
+		#time.sleep(15)
+Key_Stats()
